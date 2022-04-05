912f482b5d7173f9e93bd00d0a70e1f2d4a9dce7
Aleksei Sorotnik <sorotnik2017@mail.ru>
2022-04-05 22:28:28 +0300

    task in module 29

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def decorator_with_args_for_any_decorator(func: Callable) -> Callable:
+    """ Декоратор для декораторов. Даёт возможность
+    любому декоратору принимать произвольные аргументы.
+    """
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs) -> Callable:
+        print('Переданные арги и кварги в декоратор: {} {}'.format(args, kwargs))
+        return func
+
+    return wrapped
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *dec_args, **dec_kwargs) -> Callable:
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Callable:
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapped_func
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    """ Декоратор класса. Превращает класс в
+    синглтон (имеет один инстанс).
+    """
+
+    @functools.wraps(cls)
+    def wrapped(*args, **kwargs):
+        pass
+    return wrapped
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
