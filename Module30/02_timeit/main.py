6ed3cc36828cd94510f240bf9da3e675802c779e
Aleksei Sorotnik <sorotnik2017@mail.ru>
2022-04-09 11:12:41 +0300

    task in module 30

--- Module30/02_timeit/main.py
+++ Module30/02_timeit/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import timeit
+
+res: float = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+res_2: float = timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000)
+res_3: float = timeit.timeit('"-".join(map(lambda x: str(x), list(range(1, 100))))', number=10000)
+
+print('Время работы с использованием генератора: {time}'.format(time=res))
+print('Время работы с использованием list comprehensions: {time}'.format(time=res_2))
+print('Время работы с использованием функции map: {time}'.format(time=res_3))
--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+from typing import List
+
+# Вариант 1
+# prime_numbers: List[int] = list(filter(lambda x: 0 not in [x % y for y in range(2, x)] and x > 1, list(range(0, 1000))))
+# print(list(prime_numbers))
+
+
+# Вариант 2
+
+def get_prime() -> list:
+    """ Функция. Возращает список из простых
+    чисел от 0 до 1000"""
+
+    is_prime: bool = True
+    prime_numbers: List[int] = []
+
+    for number in range(1000):
+        for divider in range(2, number):
+            if number % divider == 0:
+                is_prime = False
+                break
+        if is_prime and number > 1:
+            prime_numbers.append(number)
+        else:
+            is_prime = True
+
+    return prime_numbers
+
+
+if __name__ == '__main__':
+    get_prime()
